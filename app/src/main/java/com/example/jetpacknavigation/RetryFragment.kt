eb2013179471cc27e560052107f3fe5695f518ac
Zidan <zidanali111@gmail.com>
2023-06-28 11:42:53 +0530

    retry fragment added.

---
+++ app/src/main/java/com/example/jetpacknavigation/RetryFragment.kt
@@ -0,0 +1,25 @@
+package com.example.jetpacknavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.jetpacknavigation.databinding.FragmentRetryBinding
+
+class RetryFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentRetryBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_retry, container, false)
+
+        return binding.root
+    }
+
+}
