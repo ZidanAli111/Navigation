74a2e536733f84264d7f6534c5e3702f29031131
Zidan <zidanali111@gmail.com>
2023-06-29 11:01:31 +0530

    about menu added.

---
+++ app/src/main/java/com/example/jetpacknavigation/AboutFragment.kt
@@ -0,0 +1,23 @@
+package com.example.jetpacknavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.jetpacknavigation.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentAboutBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false)
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/jetpacknavigation/HomeFragment.kt
+++ app/src/main/java/com/example/jetpacknavigation/HomeFragment.kt
@@ -2,11 +2,15 @@ package com.example.jetpacknavigation
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.jetpacknavigation.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -24,7 +28,21 @@ class HomeFragment : Fragment() {
                 .navigate(HomeFragmentDirections.actionHomeFragmentToAddFragment())
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            requireView().findNavController()
+        ) || super.onContextItemSelected(item)
     }
 }
